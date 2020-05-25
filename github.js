978ec26d1ff14c2740e8466ef6e26914e138cea8
avamoradi <60700386+avamoradi@users.noreply.github.com>
2020-05-25 16:55:20 +0200

    Add files via upload

---
+++ github.js
@@ -0,0 +1,22 @@
+class Github {
+  constructor() {
+    this.client_id = 'db2bc9c8852e9d106227';
+    this.clien_secret = 'f1ef0679710d2ffd67a3ab3dcad6ce811ad3eb74';
+    this.repos_count = 5;
+    this.repos_sort = 'created: asc'
+  }
+
+  async getUser(user) {
+    const profileResponse = await fetch(`https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`);
+
+    const repoResponse = await fetch(`https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}&client_id=${this.client_id}&client_secret=${this.client_secret}`);
+
+    const profile = await profileResponse.json();
+    const repos = await repoResponse.json();
+
+    return {
+      profile,
+      repos
+    }
+  }
+}
